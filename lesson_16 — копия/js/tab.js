c2d25deacb86f42a30794f301fe01fc718f909a8
alexxlefa <alexxlefa@gmail.com>
2017-05-14 22:39:07 +0300

    удалить

--- lesson_16 — копия/js/tab.js
+++
@@ -1,9 +0,0 @@
-var tab = $('.tab');
-tab.click(function(){
-	console.log('Клик по табу');
-var target = $(this).attr('data-target');
-	console.log('Закладка' + target);
-$('.tab-content').hide();
-$('.'+target).show();
-
-});
